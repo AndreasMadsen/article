27c57adb2bbc18ee186d6fb52534db8a68422f44
Andreas Madsen <amwebdk@gmail.com>
2013-05-17 16:11:58 +0200

    [tool] add a simple string diff to the analyse tool

--- test/analyse.js
+++ test/analyse.js
@@ -10,6 +10,7 @@ if (process.argv.length < 3) {
   return console.error('node analyse.js key');
 }
 
+var key = process.argv[2];
 var href = null;
 for (var i = 0, l = datamap.length; i < l; i++) {
   if (datamap[i].key === key) {
@@ -22,14 +23,74 @@ if (href === null) {
   return console.error('Cound not find test file');
 }
 
-var key = process.argv[2];
-fs.createReadStream(
-  path.resolve(__dirname, 'reallife', 'source', key + '.html')
-).pipe(article(href, function (err, result) {
-  console.log();
-  console.log(util.inspect(result, {
-    colors: true,
-    depth: Infinity
-  }));
-  console.log();
-}));
+function highlight(char) {
+ return '\x1B[7m' + char + '\x1B[27m';
+}
+
+function spaces(n) {
+  var s = '';
+  for (var i = 0; i < n; i++) {
+    s += ' ';
+  }
+  return s;
+}
+
+function alignStrings(a, b) {
+  if (a.length < b.length) {
+    a = spaces(b.length - a.length) + a;
+  } else if (b.length < a.length) {
+    b = spaces(a.length - b.length) + b;
+  }
+
+  return [a, b];
+}
+
+function stringDiff(labalA, a, labalB, b) {
+  var text = alignStrings(
+    labalA + ' (' + (new Buffer(a).length) + ') :',
+    labalB + ' (' + (new Buffer(b).length) + ') :'
+  );
+  var l = Math.max(a.length, b.length);
+  for (var i = 0; i < l; i++) {
+    if (i >= a.length) {
+      text[0] += highlight(' ');
+    } else if (i >= b.length) {
+      text[1] += highlight(' ');      
+    } else if (a[i] === b[i]) {
+      text[0] += a[i];
+      text[1] += b[i];
+    }  else {
+      text[0] += highlight(a[i]);
+      text[1] += highlight(b[i]);
+    }
+  }
+  
+  return text.join('\n');
+}
+
+fs.readFile(
+  path.resolve(__dirname, 'reallife', 'expected', key + '.json'),
+  function (err, expected) {  
+    if (err) throw err;
+    expected = JSON.parse(expected);
+
+    fs.createReadStream(
+      path.resolve(__dirname, 'reallife', 'source', key + '.html')
+    ).pipe(article(href, function (err, result) {
+      if (err) throw err;
+
+      console.log('analysed:');
+      console.log('------');
+      console.log(util.inspect(result, {
+        colors: true,
+        depth: Infinity
+      }));
+      console.log('------');
+      if (result.title !== expected.title) {
+        console.log(stringDiff('acutal', result.title, 'expected', expected.title));
+      } else {
+        console.log('The actual result matched the expected result');
+      }
+    }));
+  }
+);
