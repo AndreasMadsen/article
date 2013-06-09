b6ad58a655d5f6cacb6ddd67206fbbe4a88804b9
Andreas Madsen <amwebdk@gmail.com>
2013-06-09 15:28:52 +0200

    [feature] introduce microdata subspace

--- test/internal/treebuilder.js
+++ test/internal/treebuilder.js
@@ -55,7 +55,7 @@ test('handlers emits in order', function (t) {
       validateTree(root, {
         type: 'root',
         position: 0,
-        density: 3.125,
+        density: 25/6,
 
         children: [
           {
@@ -63,7 +63,7 @@ test('handlers emits in order', function (t) {
             tagname: 'h1',
             attr: { id: '1' },
             position: 1,
-            density: 6.5,
+            density: 13,
 
             children: [
               {
@@ -98,7 +98,7 @@ test('handlers emits in order', function (t) {
             tagname: 'div',
             attr: { id: '3' },
             position: 5,
-            density: 4,
+            density: 6,
             
             children: [
               {
@@ -199,5 +199,151 @@ test('handlers emits in order', function (t) {
     '<script>/* some crazy code we do not care about */</script>\n' +
     '<style>/* some crazy code we do not care about */</style>\n' +
     '<div></div>'
+  ).pipe(builder);
+});
+
+test('handlers manages microdata', function (t) {
+  function validateTree(node, expected, parent) {
+    parent = parent || null;
+
+    t.equal(node.parent, parent);
+
+    t.equal(node.type, expected.type);
+    t.equal(node.key, expected.hasOwnProperty('key') ? expected.key : null);
+    t.equal(node.value, expected.hasOwnProperty('value') ? expected.value : null);
+    t.equal(node.scope, expected.hasOwnProperty('scope') ? expected.scope : null);
+    
+    var i, l, p, r;
+    
+    if (expected.hasOwnProperty('scropes')) {
+      var scropes = Object.keys(node.scropes);
+      t.deepEqual(scropes, Object.keys(expected.scropes));
+
+      for (i = 0, l = scropes.length; i < l; i++) {
+        t.equal(node.scropes[scropes[i]].length, expected.scropes[scropes[i]].length);
+
+        for (p = 0, r = node.scropes[scropes[i]].length; p < r; p++) {
+          validateTree(
+            node.scropes[scropes[i]][p],
+            expected.scropes[scropes[i]][p],
+            node.scropes[scropes[i]][p].parent // skip parent test
+          );
+        }
+      }
+    } else {
+      t.equal(node.scropes, null);
+    }
+
+    if (expected.hasOwnProperty('properties')) {
+      var properties = Object.keys(node.properties);
+      t.deepEqual(properties, Object.keys(expected.properties));
+
+      for (i = 0, l = properties.length; i < l; i++) {
+        t.equal(node.properties[properties[i]].length, expected.properties[properties[i]].length);
+
+        for (p = 0, r = node.properties[properties[i]].length; p < r; p++) {
+          validateTree(
+            node.properties[properties[i]][p],
+            expected.properties[properties[i]][p],
+            node
+          );
+        }
+      }
+    } else {
+      t.equal(node.properties, null);
+    }
+  }
+
+  var builder = new Tree({
+    element: function (node) { },
+    end: function () {
+      validateTree(builder._tree.micro, {
+        type: 'root',
+        parent: null,
+
+        properties: {},
+
+        scropes: {
+          'http://data-vocabulary.org/Event': [{
+            type: 'scope',
+            parent: '__root__',
+            scope: 'http://data-vocabulary.org/Event',
+            key: false,
+
+            properties: {
+              url: [{
+                type: 'item',
+                key: 'url',
+                value: 'http://example.test'
+              }],
+              summary: [{
+                type: 'item',
+                key: 'summary',
+                value: 'Main summary'
+              }],
+              photo: [{
+                type: 'item',
+                key: 'photo',
+                value: '/picture.jpg'
+              }],
+              description: [{
+                type: 'item',
+                key: 'description',
+                value: 'Crazy description'
+              }],
+              startDate: [{
+                type: 'item',
+                key: 'startDate',
+                value: '2010-06-13'
+              }],
+              location: [{
+                type: 'scope',
+                parent: 'http://data-vocabulary.org/Event',
+                scope: 'http://data-vocabulary.org/Organization',
+                key: 'location',
+
+                properties: {
+                  name: [{
+                    type: 'item',
+                    key: 'name',
+                    value: 'Here'
+                  }]
+                }
+              }]
+            }
+          }],
+          'http://data-vocabulary.org/Organization': [{
+            type: 'scope',
+            scope: 'http://data-vocabulary.org/Organization',
+
+            key: 'location',
+            properties: {
+              name: [{
+                type: 'item',
+                key: 'name',
+                value: 'Here'
+              }]
+            }
+          }]
+        }
+      });
+
+      t.equal(builder._tree.micro.node, builder._tree);
+      t.end();
+    }
+  });
+
+  startpoint(
+    '<div itemscope itemtype="http://data-vocabulary.org/Event">\n' +
+    '  <a href="http://example.test" itemprop="url">\n' +
+    '    <span itemprop="summary">Main summary</span>\n' +
+    '  </a>\n' +
+    '  <img itemprop="photo" src="/picture.jpg">\n' +
+    '  <span itemprop="description">Crazy description</span>\n' +
+    '  <time itemprop="startDate" datetime="2010-06-13">June 13th, 2010</time>\n' +
+    '  <span itemprop="location" itemscope itemtype="http://data-vocabulary.org/Organization">\n' +
+    '    <span itemprop="name">Here</span>\n' +
+    '  </span>\n' +
+    '</div>'
   ).pipe(builder);
 });
