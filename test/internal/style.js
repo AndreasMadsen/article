459645157a6a49dbe05457536f9a8d3f0b16aa30
Andreas Madsen <amwebdk@gmail.com>
2013-06-10 16:32:33 +0200

    [feature] introduce a style parser

---
+++ test/internal/style.js
@@ -0,0 +1,22 @@
+
+var test = require('tap').test;
+var styleParser = require('../../lib/helpers-dom.js').styleParser;
+
+test('test style parser with space everywhere', function (t) {
+  t.deepEqual(styleParser(' a : v1 ; b : v2 '), {
+    a: 'v1',
+    b: 'v2'
+  });
+  t.end();
+});
+
+test('test style parser with malformated attribute', function (t) {
+  t.deepEqual(styleParser(' a : v1 ; b : '), {
+    a: 'v1',
+    b: ''
+  });
+  t.deepEqual(styleParser(' a : v1 ; b '), {
+    a: 'v1'
+  });
+  t.end();
+});
