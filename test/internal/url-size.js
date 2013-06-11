671b6cca54d008fa4b41ac11689dd1e799ae029d
Andreas Madsen <amwebdk@gmail.com>
2013-06-11 20:48:52 +0200

    [feature] add an estimating ImageSize object

---
+++ test/internal/url-size.js
@@ -0,0 +1,37 @@
+
+var test = require('tap').test;
+var ImageAlgoritme = require('../../lib/fase-1/image.js');
+var sizeFromUrl = ImageAlgoritme.prototype._sizeFromUrl;
+
+function expectedSize(width, height) {
+  return {
+    width: width,
+    height: height,
+    area: width * height || null
+  };
+}
+
+test('width times height match', function (t) {
+  t.deepEqual(sizeFromUrl('http://example.test/cat/100x200.jpg'), expectedSize(100, 200));
+  t.deepEqual(sizeFromUrl('http://example.test/cat/16x9.jpg'), expectedSize(null, null));
+  t.end();
+});
+
+test('short width and hight query names', function (t) {
+  t.deepEqual(sizeFromUrl('http://example.test/?w=100&h=200'), expectedSize(100, 200));
+  t.deepEqual(sizeFromUrl('http://example.test/?w=100'), expectedSize(100, 62));
+  t.deepEqual(sizeFromUrl('http://example.test/?h=200'), expectedSize(324, 200));
+  t.end();
+});
+
+test('long width and hight query names', function (t) {
+  t.deepEqual(sizeFromUrl('http://example.test/?width=100&height=200'), expectedSize(100, 200));
+  t.deepEqual(sizeFromUrl('http://example.test/?width=100'), expectedSize(100, 62));
+  t.deepEqual(sizeFromUrl('http://example.test/?height=200'), expectedSize(324, 200));
+  t.end();
+});
+
+test('Returns null size when there are no image', function (t) {
+  t.deepEqual(sizeFromUrl('http://example.test'), expectedSize(null, null));
+  t.end();
+});
